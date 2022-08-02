fc671709dbf5b9a1f3ac83954973997cb28e1075
Atanu Pradhan <42454254+mejda07@users.noreply.github.com>
2022-08-02 10:07:55 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,110 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "The first page of a website is called _____.",
+    answer: "Home Page",
+    options: ["Design Page", "Home Page", "Front Page", "Main Page"],
+  },
+  {
+    numb: 7,
+    question: "The Head tag is used for?",
+    answer: "All the answers are true",
+    options: [
+      "Writing CSS styles",
+      "Writing Javascript",
+      "Including CSS and JS files",
+      "All the answers are true",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Choose the correct HTML tag for a large title.",
+    answer: "H1",
+    options: ["H1", "Heading", "Head", "H6"],
+  },
+  {
+    numb: 9,
+    question: "The HTML document contains a root tag called ____.",
+    answer: "HTML",
+    options: ["HEAD", "Title", "Body", "HTML"],
+  },
+  {
+    numb: 10,
+    question: "Which type of JavaScript language is _____?",
+    answer: "Object-based",
+    options: [
+      "Object-oriented",
+      "Object-based",
+      "Functional programming",
+      "All of the above",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
